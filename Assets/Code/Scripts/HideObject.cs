91a0224408be55ec6b28f18e70a5dc76bb8cbeee
SamiSha <Sami.shakkour@hotmail.com>
2023-01-03 16:02:50 +0200

    now has bool to check if hidden

--- Assets/Code/Scripts/HideObject.cs
+++ Assets/Code/Scripts/HideObject.cs
@@ -6,11 +6,15 @@ public class HideObject : MonoBehaviour
 {
     [Tooltip("Hide the gameobject on run time, assuming we are handling it somewhere else.")]
     public bool _hidden;
-    void Awake()
-    {
-        if(_hidden) Hide();
-    }
+    void Awake() { if(_hidden) Hide(); }
     public void Hide() => SetHidden(true);
     public void Unhide() => SetHidden(false);
-    public void SetHidden(bool _hidden = false) => transform.ToggleHidden(_hidden);
+    public void SetHidden(bool _hidden = false)
+    {
+        transform.ToggleHidden(_hidden);
+        this._hidden = _hidden;
+    }
+    public bool IsHidden() { return _hidden; }
+
+    void OnValidate() => SetHidden(_hidden);
 }
