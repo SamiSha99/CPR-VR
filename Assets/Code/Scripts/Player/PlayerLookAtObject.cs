e11b67b7c68e8b0353ff9c540580a2d7893fe9fc
SamiSha <Sami.shakkour@hotmail.com>
2023-01-09 16:14:50 +0200

    Moved PlayerLookAtObject event to right folder, now functional

--- Assets/Code/Scripts/Player/PlayerLookAtObject.cs
+++ Assets/Code/Scripts/Player/PlayerLookAtObject.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// The look at target requires a collider or rigidbody!!!!
 public class PlayerLookAtObject : MonoBehaviour
 {
     public Camera _camera;
     public GameObject lookingAtObject;
     public XREvents xrEvents;
+    public bool _Debugging;
+    private LineRenderer lr;
+    [SerializeField] private LineRenderer lineRendererGameObject;
     // Update is called once per frame
     void Update()
     {
         Transform camTrans = _camera.transform;
+        Vector3 camPos = _camera.transform.position, camDir = _camera.transform.forward;
         RaycastHit hit;
-        if(Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity) && IsHitValid(hit))
+        if(_Debugging)
+        {
+            if(lr == null) lr = Instantiate(lineRendererGameObject).GetComponent<LineRenderer>();
+            
+            //bool bhit = Physics.Raycast(camPos, camDir, out hit, Mathf.Infinity, ~(1 << 30));
+            lr.startColor = Color.red;
+            lr.endColor = Color.red;
+            lr.startWidth = 0.01f;
+            lr.endWidth = 0.02f;
+            
+            lr.SetPosition(0, camPos + Vector3.down * 0.05f);
+            lr.SetPosition(1, camDir * 50);
+            //Debug.DrawLine(camPos, bhit ? hit.point : camDir, Color.red);
+        }
+        
+        if(Physics.Raycast(camPos, camDir, out hit, Mathf.Infinity) && IsHitValid(hit))
         {
             // what we previously were looking at
             // avoid first unlook as "null" 
