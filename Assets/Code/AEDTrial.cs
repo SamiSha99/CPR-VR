a839c7257f7f39350bbd73b6085ad50bb552bcdb
SamiSha <Sami.shakkour@hotmail.com>
2023-02-19 16:02:59 +0200

    AED test that works

--- Assets/Code/AEDTrial.cs
+++ Assets/Code/AEDTrial.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class AEDTrial : MonoBehaviour
 {
+    private int appliedPatches;
+    //AudioClip ;
+    public float analyzerTimer = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,21 @@ public class AEDTrial : MonoBehaviour
     void Update()
     {
         
+    }
+    public void OnPatchApplied()
+    {
+        appliedPatches++;
+        if(appliedPatches >= 2) OnAllPatchesApplied();
+        
+    }
+    // Handled in inspector!!!
+    public void OnAllPatchesApplied()
+    {
+        Util.Invoke(this, () => OnWaitingForAED(), 5.0f);
+    }
+
+    public void OnWaitingForAED()
+    {
+        QuestManager._Instance.CompleteCommandGoal("Wait_for_AED_Goal_Command");
     }
 }
