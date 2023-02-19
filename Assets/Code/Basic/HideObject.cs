80563a2a35149a058eff29bbc6a36778749b796b
SamiSha <Sami.shakkour@hotmail.com>
2023-02-19 16:00:34 +0200

    code cleanup

--- Assets/Code/Basic/HideObject.cs
+++ Assets/Code/Basic/HideObject.cs
@@ -7,15 +7,14 @@ public class HideObject : MonoBehaviour
     [Tooltip("Hide the gameobject on run time, assuming we are handling it somewhere else.")]
     public bool _hidden;
     void Awake() { if(_hidden) Hide(); }
-    public void Hide() => SetHidden(true);
-    public void Unhide() => SetHidden(false);
     public void SetHidden(bool _hidden = false)
     {
         transform.ToggleHidden(_hidden);
         this._hidden = _hidden;
     }
+    public void Hide() => SetHidden(true);
+    public void Unhide() => SetHidden(false);
     public void ToggleHidden() => SetHidden(!_hidden);
     public bool IsHidden() { return _hidden; }
-
     void OnValidate() => SetHidden(_hidden);
 }
