e8b4efe2d4b2ded048b4ae38ae67df29546b0ca4
SamiSha <Sami.shakkour@hotmail.com>
2023-03-28 16:01:36 +0300

    Finished settings manager

--- Assets/Code/UI/SettingsManager.cs
+++ Assets/Code/UI/SettingsManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SettingsManager : MonoBehaviour
 {
@@ -9,9 +11,86 @@ public class SettingsManager : MonoBehaviour
     public float textToSpeechVolume = 0.8f;
     public int loops = 3;
 
-    public void LoadPrefs()
-    {
+    const string OPTION_TTS_VOLUME = "OPTION_TTS_VOLUME";
+    const string OPTION_CENTIMETERS = "OPTION_CENTIMETERS";
+    const string OPTION_CPR_LOOPS = "OPTION_CPR_LOOPS";
 
+    public void OnSettingAdjusted(GameObject OptionRow)
+    {
+        string optionaName = OptionRow.name;
+
+        switch(optionaName)
+        {
+            case OPTION_TTS_VOLUME:
+                Slider s = OptionRow.transform.FindComponent<Slider>("OptionConfig/Slider");
+                PlayAudioAtLocation audioSample = OptionRow.transform.FindComponent<PlayAudioAtLocation>("OptionConfig/Slider");
+                
+                float volume = s.value;
+                textToSpeechVolume = volume;
+                audioSample.SetVolume(volume);
+                if(audioSample.IsPlaying())
+                {
+                    audioSample.activeAudioSource.volume = volume;
+                    break;
+                }
+                audioSample.TriggerAudio();
+                break;
+    
+            case OPTION_CENTIMETERS:
+                Toggle t = OptionRow.transform.FindComponent<Toggle>("OptionConfig/Toggle");
+                useCentimeter = t.isOn;
+                break;
+
+            case OPTION_CPR_LOOPS:
+                TMP_Dropdown d = OptionRow.transform.FindComponent<TMP_Dropdown>("OptionConfig/Dropdown");
+                loops = d.value;
+                break;
+
+            default:
+                Debug.LogWarning("This option is not defined, what is this? Ignoring: " + optionaName);
+                return;
+        }
+    }
+
+    public void LoadPrefs(GameObject options)
+    {
+        useCentimeter = Util.IntToBool(PlayerPrefs.GetInt(nameof(useCentimeter), Util.BoolToInt(useCentimeter)));
+        textToSpeechVolume = PlayerPrefs.GetFloat(nameof(textToSpeechVolume), textToSpeechVolume);
+        loops = PlayerPrefs.GetInt(nameof(loops), loops);
+        UpdateOptionsUI(options);
+    }
+
+    void UpdateOptionsUI(GameObject options)
+    {
+        List<Transform> optionRows = new List<Transform>();
+
+        options.transform.GetChildren(optionRows);
+        foreach(Transform or in optionRows)
+        {
+            string optionaName = or.gameObject.name;
+            GameObject optionRow = or.gameObject;
+            switch(optionaName)
+            {
+                case OPTION_TTS_VOLUME:
+                    Slider s = optionRow.transform.FindComponent<Slider>("OptionConfig/Slider");
+                    s.SetValueWithoutNotify(textToSpeechVolume);
+                    break;
+    
+                case OPTION_CENTIMETERS:
+                    Toggle t = optionRow.transform.FindComponent<Toggle>("OptionConfig/Toggle");
+                    t.isOn = useCentimeter;
+                    break;
+
+                case OPTION_CPR_LOOPS:
+                    TMP_Dropdown d = optionRow.transform.FindComponent<TMP_Dropdown>("OptionConfig/Dropdown");
+                    d.value = loops;
+                    break;
+
+                default:
+                    Debug.LogWarning("This option is not defined, what is this? Ignoring: " + optionaName);
+                return;
+            }
+        }
     }
 
     public void SaveChanges()
