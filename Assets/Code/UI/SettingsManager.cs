ac020236893b7e4f3c70f5cedba3aca2037a4310
SamiSha <Sami.shakkour@hotmail.com>
2023-04-25 21:47:29 +0300

    added language option

--- Assets/Code/UI/SettingsManager.cs
+++ Assets/Code/UI/SettingsManager.cs
@@ -10,11 +10,12 @@ public class SettingsManager : MonoBehaviour
     [Range(0,1)]
     public float textToSpeechVolume = 0.8f;
     public int loops = 3;
-
+    public int languageIndex;
+    
     const string OPTION_TTS_VOLUME = "OPTION_TTS_VOLUME";
     const string OPTION_CENTIMETERS = "OPTION_CENTIMETERS";
     const string OPTION_CPR_LOOPS = "OPTION_CPR_LOOPS";
-
+    const string OPTION_LANGUAGE = "OPTION_LANGUAGE";
     void Start()
     {
         LoadPrefs();
@@ -27,27 +28,26 @@ public class SettingsManager : MonoBehaviour
         switch(optionaName)
         {
             case OPTION_TTS_VOLUME:
-                Slider s = OptionRow.transform.FindComponent<Slider>("OptionConfig/Slider");
                 PlayAudioAtLocation audioSample = OptionRow.transform.FindComponent<PlayAudioAtLocation>("OptionConfig/Slider");
-                
-                float volume = s.value;
+                float volume = FindOptionConfig<Slider>(OptionRow, nameof(Slider)).value;
                 textToSpeechVolume = volume;
                 audioSample.SetVolume(volume);
                 if(audioSample.activeAudioSource != null)
                     audioSample.activeAudioSource.volume = volume;
-                if(!audioSample.IsPlaying()) audioSample.TriggerAudio(LocalizationManager.GetAsset<AudioClip>("AudioTable", "AudioVolumeTest"));
+                if(!audioSample.IsPlaying()) audioSample.TriggerAudio(LocalizationHelper.GetAsset<AudioClip>("AudioTable", "AudioVolumeTest"));
                 break;
     
             case OPTION_CENTIMETERS:
-                Toggle t = OptionRow.transform.FindComponent<Toggle>("OptionConfig/Toggle");
-                useCentimeter = t.isOn;
+                useCentimeter = FindOptionConfig<Toggle>(OptionRow, nameof(Toggle)).isOn;
                 break;
 
             case OPTION_CPR_LOOPS:
-                TMP_Dropdown d = OptionRow.transform.FindComponent<TMP_Dropdown>("OptionConfig/Dropdown");
-                loops = d.value;
+                loops = FindOptionConfig<TMP_Dropdown>(OptionRow, "Dropdown").value;
                 break;
-
+            case OPTION_LANGUAGE:
+                languageIndex = FindOptionConfig<TMP_Dropdown>(OptionRow, "Dropdown").value;
+                LocalizationHelper.SetLanguage(languageIndex);
+                break;
             default:
                 Debug.LogWarning("This option is not defined, what is this? Ignoring: " + optionaName);
                 return;
@@ -75,21 +75,26 @@ public class SettingsManager : MonoBehaviour
             switch(optionaName)
             {
                 case OPTION_TTS_VOLUME:
-                    Slider s = optionRow.transform.FindComponent<Slider>("OptionConfig/Slider");
-                    s.value = textToSpeechVolume;
-                    //s.SetValueWithoutNotify(textToSpeechVolume);
+                    Slider s = FindOptionConfig<Slider>(optionRow, nameof(Slider)); //optionRow.transform.FindComponent<Slider>("OptionConfig/Slider");
+                    s.SetValueWithoutNotify(textToSpeechVolume);
                     break;
     
                 case OPTION_CENTIMETERS:
-                    Toggle t = optionRow.transform.FindComponent<Toggle>("OptionConfig/Toggle");
+                    Toggle t = FindOptionConfig<Toggle>(optionRow, nameof(Toggle)); //optionRow.transform.FindComponent<Toggle>("OptionConfig/Toggle");
                     t.isOn = useCentimeter;
                     break;
 
                 case OPTION_CPR_LOOPS:
-                    TMP_Dropdown d = optionRow.transform.FindComponent<TMP_Dropdown>("OptionConfig/Dropdown");
+                    TMP_Dropdown d = FindOptionConfig<TMP_Dropdown>(optionRow, "Dropdown");
                     d.value = loops;
                     break;
-
+                
+                case OPTION_LANGUAGE:
+                    TMP_Dropdown dropDownLang = FindOptionConfig<TMP_Dropdown>(optionRow, "Dropdown");
+                    dropDownLang.AddOptions(LocalizationHelper.GetAvailableLanguagesAsNames());
+                    dropDownLang.value = LocalizationHelper.GetSelectedLanguageIndex();
+                    break;
+                
                 default:
                     Debug.LogWarning("This option is not defined, what is this? Ignoring: " + optionaName);
                 return;
@@ -102,10 +107,17 @@ public class SettingsManager : MonoBehaviour
         PlayerPrefs.SetInt(nameof(useCentimeter), Util.BoolToInt(useCentimeter));
         PlayerPrefs.SetFloat(nameof(textToSpeechVolume), textToSpeechVolume);
         PlayerPrefs.SetInt(nameof(loops), loops);
+        PlayerPrefs.SetInt(nameof(languageIndex), languageIndex);
         PlayerPrefs.Save();
     }
     public void ResetToDefault()
     {
         PlayerPrefs.DeleteAll();
+    }
+
+    //optionRow.transform.FindComponent<TMP_Dropdown>("OptionConfig/Dropdown");
+    private T FindOptionConfig<T>(GameObject optionRow, string goName) where T : UnityEngine.Component
+    {
+        return optionRow.transform.FindComponent<T>("OptionConfig/"+goName);
     }
 }
