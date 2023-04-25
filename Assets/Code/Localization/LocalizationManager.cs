ca21d01cfd76b456960e46df7e7b30d3500b0049
SamiSha <Sami.shakkour@hotmail.com>
2023-04-25 21:46:57 +0300

    LocalizationManager now runs on runtime
     flipping all canvases in scene

---
+++ Assets/Code/Localization/LocalizationManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+using UnityEngine.UI;
+using TMPro;
+
+public class LocalizationManager : MonoBehaviour
+{
+    public bool UIFlipped;
+    public LocalizationManager _Instance;
+    void Awake() => _Instance = this;
+    void Start()
+    {
+        LocalizationSettings.SelectedLocaleChanged += OnLanguageChanged;
+        //LocalizationHelper.SetLanguage(PlayerPrefs.GetInt(nameof(SettingsManager.languageIndex), 1));
+    }
+    void OnDestroy() => LocalizationSettings.SelectedLocaleChanged -= OnLanguageChanged;
+    void OnEnable() => LocalizationSettings.SelectedLocaleChanged += OnLanguageChanged;
+    void OnDisable() => LocalizationSettings.SelectedLocaleChanged -= OnLanguageChanged;
+    void OnLanguageChanged(Locale selectedLanguage) => FlipUI();
+    void FlipUI()
+    {
+        if(UIFlipped == LocalizationHelper.UsingRightToLeftLanguage()) return;
+        UIFlipped = !UIFlipped;
+        LocalizationHelper.FlipCanvas();
+    }
+}
+
