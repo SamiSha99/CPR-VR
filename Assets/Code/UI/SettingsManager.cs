e441cf8c6e013ffd0bea39424398bd444e809bfe
SamiSha <Sami.shakkour@hotmail.com>
2023-04-30 15:27:12 +0300

    Localized Settings Menu WIP

--- Assets/Code/UI/SettingsManager.cs
+++ Assets/Code/UI/SettingsManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Localization.Settings;
+using UnityEngine.Localization;
 
 public class SettingsManager : MonoBehaviour
 {
@@ -11,13 +13,14 @@ public class SettingsManager : MonoBehaviour
     public float textToSpeechVolume = 0.8f;
     public int loops = 3;
     public int languageIndex;
-    
+    public GameObject optionsGameObject;
     const string OPTION_TTS_VOLUME = "OPTION_TTS_VOLUME";
     const string OPTION_CENTIMETERS = "OPTION_CENTIMETERS";
     const string OPTION_CPR_LOOPS = "OPTION_CPR_LOOPS";
     const string OPTION_LANGUAGE = "OPTION_LANGUAGE";
     void Start()
     {
+        LocalizationSettings.SelectedLocaleChanged += OnLanguageChanged;
         LoadPrefs();
     }
 
@@ -34,7 +37,15 @@ public class SettingsManager : MonoBehaviour
                 audioSample.SetVolume(volume);
                 if(audioSample.activeAudioSource != null)
                     audioSample.activeAudioSource.volume = volume;
-                if(!audioSample.IsPlaying()) audioSample.TriggerAudio(LocalizationHelper.GetAsset<AudioClip>("AudioTable", "AudioVolumeTest"));
+                if(!audioSample.IsPlaying())
+                {
+                    AudioClip testVolumeClip = LocalizationHelper.GetAsset<AudioClip>("AudioTable", "AudioVolumeTest");
+                    if(testVolumeClip != null)
+                        audioSample.TriggerAudio(testVolumeClip);
+                    else
+                        audioSample.TriggerAudio();
+
+                }
                 break;
     
             case OPTION_CENTIMETERS:
@@ -71,34 +82,42 @@ public class SettingsManager : MonoBehaviour
         foreach(Transform or in optionRows)
         {
             string optionaName = or.gameObject.name;
+            TextMeshProUGUI tmpSettingName = or.FindComponent<TextMeshProUGUI>("OptionName");
+            string localizationCommand = "";
             GameObject optionRow = or.gameObject;
             switch(optionaName)
             {
                 case OPTION_TTS_VOLUME:
+                    localizationCommand = "MainMenu.SpeechVolume";
                     Slider s = FindOptionConfig<Slider>(optionRow, nameof(Slider)); //optionRow.transform.FindComponent<Slider>("OptionConfig/Slider");
                     s.SetValueWithoutNotify(textToSpeechVolume);
                     break;
     
                 case OPTION_CENTIMETERS:
+                    localizationCommand = "MainMenu.SwapInches";
                     Toggle t = FindOptionConfig<Toggle>(optionRow, nameof(Toggle)); //optionRow.transform.FindComponent<Toggle>("OptionConfig/Toggle");
                     t.isOn = useCentimeter;
                     break;
 
                 case OPTION_CPR_LOOPS:
+                    localizationCommand = "MainMenu.CPRLoops";
                     TMP_Dropdown d = FindOptionConfig<TMP_Dropdown>(optionRow, "Dropdown");
                     d.value = loops;
                     break;
                 
                 case OPTION_LANGUAGE:
+                    localizationCommand = "MainMenu.Language";
                     TMP_Dropdown dropDownLang = FindOptionConfig<TMP_Dropdown>(optionRow, "Dropdown");
+                    dropDownLang.ClearOptions();
                     dropDownLang.AddOptions(LocalizationHelper.GetAvailableLanguagesAsNames());
                     dropDownLang.value = LocalizationHelper.GetSelectedLanguageIndex();
                     break;
                 
                 default:
                     Debug.LogWarning("This option is not defined, what is this? Ignoring: " + optionaName);
-                return;
+                    return;
             }
+            LocalizationHelper.LocalizeTMP(localizationCommand, tmpSettingName);
         }
     }
 
@@ -119,5 +138,10 @@ public class SettingsManager : MonoBehaviour
     private T FindOptionConfig<T>(GameObject optionRow, string goName) where T : UnityEngine.Component
     {
         return optionRow.transform.FindComponent<T>("OptionConfig/"+goName);
+    }
+
+    void OnLanguageChanged(Locale selectedLocale)
+    {
+        LoadPrefs(optionsGameObject);
     }
 }
