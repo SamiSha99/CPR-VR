cadce372d2a3e5275d6b5924fe2e5773269572f8
SamiSha <Sami.shakkour@hotmail.com>
2023-06-19 12:29:26 +0300

    should work with hand tracking now

--- Assets/Code/Player/AnimateHandOnInput.cs
+++ Assets/Code/Player/AnimateHandOnInput.cs
@@ -11,16 +11,25 @@ public class AnimateHandOnInput : MonoBehaviour
     {
         if(handAnimator == null) Debug.LogWarning("handAnimator is not specified, returned " + handAnimator);
     }
+
+    void OnEnable()
+    {
+        if(pinchAnimationAction != null) pinchAnimationAction.action.Reset();
+        if(gripAnimationAction != null) gripAnimationAction.action.Reset();
+    }
+
     void Update()
     {
         if(pinchAnimationAction != null)
         {
-            float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+            float triggerValue = 0;
+            if(pinchAnimationAction.action.IsPressed()) triggerValue = pinchAnimationAction.action.ReadValue<float>();
             AnimateHand("Trigger", triggerValue);
         }
         if(gripAnimationAction != null)
         {
-            float gripValue = gripAnimationAction.action.ReadValue<float>();
+            float gripValue = 0;
+            if(gripAnimationAction.action.IsPressed()) gripValue = gripAnimationAction.action.ReadValue<float>();
             AnimateHand("Grip", gripValue);
         }
     }
