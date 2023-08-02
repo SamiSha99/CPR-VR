b267cf76b7f2e10825b8835b061c9c8de77e9ab4
SamiSha <Sami.shakkour@hotmail.com>
2023-08-02 15:49:38 +0300

    WIP subtitles system?

---
+++ Assets/Code/SubtitleManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// To-do: Subtitles that manages prompts and more, all set here, also move the mistake prompts to here?
+public class SubtitleManager : MonoBehaviour
+{
+    public static SubtitleManager _Instance;
+
+    void Awake() => _Instance = this;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
